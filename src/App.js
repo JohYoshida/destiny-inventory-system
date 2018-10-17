cba09a4c2ae1ee597bca127f0bfa0dcb5e610ae8
Joh Yoshida <johyoshida@gmail.com>
2018-10-16 22:18:20 -0700

    Pass weapons and armors to respective components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import React, { Component } from 'react';
 import Weapons from "./components/Weapons";
 import Armors from "./components/Armors";
+import { weapons, armor } from "./inventory";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      weapons,
+      armor,
+    };
+  }
+
   render() {
     return (
       <div className="character">
         <div className="inventory">
-          <Weapons />
+          <Weapons weapons={this.state.weapons} />
           <div className="player-model">
             <p>Player Model</p>
           </div>
-          <Armors />
+          <Armors armor={this.state.armor} />
         </div>
       </div>
     );
